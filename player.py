2b2becf8de342e1335511515e390652b5212932b
tsazo <tsazo@andover.edu>
2022-08-02 22:18:15 -0400

    Fix bug

--- player.py
+++ player.py
@@ -32,17 +32,17 @@ class HumanPlayer(Player):
         val = None
 
         while not valid_square:
-            square = input(self.letter + '\'s turn. Input move (0-9): ')
+            square = input(self.letter + '\'s turn. Input move (0-8): ')
             # we're going to check that this is a correct value by trying to cast
             # it to an integer, and if it's not, then we say its invalid.
             # if that spot is not availble on the board, we also say it's invalid
 
             try:
-              val = int(square)
-              if val not in game.available_moves():
-                raise ValueError
-              valid_square = True
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
             except ValueError:
-              print('Invalid square. Try again.')
-    
+                print('Invalid square. Try again.')
+
         return val
